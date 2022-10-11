9e44df646e4a0e8d84e0dbcbcd750d9c8a708e45
arihanthadgali <109871543+arihanthadgali@users.noreply.github.com>
2022-10-11 11:32:55 +0530

    second commit

---
+++ client/myapp/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="Music-title">
+        Music
+      </Link>
+      <ul>
+        <input type="text" placeholder="search"></input>
+        <button type="submit">submit</button>
+        <CustomLink to="/home">Home</CustomLink>
+        <CustomLink to="/songs">Songs</CustomLink>
+        <CustomLink to="/artists">Artists</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
--- client/myapp/src/pages/NotFound.js
+++ client/myapp/src/pages/NotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
-function NOtFound() {
+function NotFound() {
   return <h1>NOtFound</h1>;
 }
 
-export default NOtFound;
+export default NotFound;
